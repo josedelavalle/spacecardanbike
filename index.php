b1d151a3b7067ea2ebafe1527d9b1fb81cfa8f6e
Jose DeLavalle <me@slickwebstudio.com>
2016-10-05 10:03:14 -0400

    Space Cardan Bike

---
+++ index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Space Cardan Bike USA</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="space cardan bike usa">
+        <meta name="keywords" content="space,cardan,bike,bicycle,rental,drive shaft">
+        <meta name="author" content="jose delavalle">
+        <meta name="format-detection" content="telephone=no">
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		
+        <style type="text/css">
+    
+</style>
+
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+                    <h1 id="logo"><a href="#">Space Cardan Bike USA</a>&nbsp;&nbsp;
+                        <a href="https://www.facebook.com/spacecardanbikeusa/" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a>&nbsp;&bull;&nbsp;
+                        <a href="https://www.instagram.com/g.brin/" class="icon alt fa-instagram"><span class="label">Instagram</span></a>&nbsp;&bull;&nbsp;
+                        <a href="https://www.linkedin.com/in/gina-brin-2488a511a" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a>
+                    </h1>
+                    
+					<nav id="nav">
+						<ul>
+						
+							<li><a href="index.php">Home</a></li>
+							<li><a href="dark/index.html">Parts Gallery</a></li>
+							<li>
+								<a href="#">Welcome</a>
+								<ul>
+									<li><a href="#one">Technology</a></li>
+							    	<li><a href="#two">Quality</a></li>
+								    <li><a href="#three">Leadership</a></li>
+				            		<li><a href="#four">Reliability</a></li>
+				            		<li><a href="#five">Contact Us</a></li>
+									<!--<li>
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#one">Leadership</a></li>
+											<li><a href="#two">Quality</a></li>
+											<li><a href="#three">Technology</a></li>
+											<li><a href="#four">Reliability</a></li>
+										</ul>
+									</li>-->
+								</ul>
+							</li>
+							<!--<li><a href="elements.html">Elements</a></li>
+							<li><a href="#" class="button special">Sign Up</a></li>-->
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<div class="content">
+					    
+						<header>
+						<div class="zoom_img">
+						<img src="images/driveShaft2.png" />
+						</div>
+							<h2>Space Cardan Bike USA</h2>
+                            <p>modern, chainless bicycle design, <br>
+							smart, elegant rental soultions</p>
+						</header>
+						<span class="image"><img src="images/pic01.jpg" alt="" /></span>
+					</div>
+					<a href="#one" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- One -->
+				<section id="one" class="spotlight style1 bottom">
+					<span class="image fit main"><img src="images/pic02.jpg" alt="" /></span>
+					<div class="content">
+						<div class="container">
+							<div class="row">
+								<div class="4u 12u$(medium)">
+									<header>
+										<h2>TECHNOLOGY</h2>
+										<p>Space Cardan Bike USA</p>
+									</header>
+								</div>
+								<div class="4u 12u$(medium)">
+									<p>The bicycle was invented in 1817.  And in its nearly 200 year history, its design has remained largely unchanged.  Innovation in this market is long overdue, and we are here to change that.  Space Cardan's revolutionary drive shaft system will do just that.
+									</p>
+								</div>
+								<div class="4u$ 12u$(medium)">
+									<p>Any child with a bicycle can tell you, the number one problem they encounter is always with their bike's chain.  It can detach from the gear, entangle itself, or even break altogether.  The danger of rust is real.  This is a source of much hassle, and unneccessary maintenance costs.  Our drive shafts, incorporated into all Space Cardan bicycles eliminates both of these issues.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Two -->
+				<section id="two" class="spotlight style2 right">
+					<span class="image fit main"><img src="images/pic03.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>QUALITY</h2>
+							<p>Space Cardan Bike USA</p>
+						</header>
+						<p>Space Cardan Bike, USA has become synonymous with the highest level of quality craftsmanship. All backed by our 5 year guarantee.</p>
+						<!--<ul class="actions">
+							<li><a href="#" class="button">Learn More</a></li>
+						</ul>-->
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Three -->
+				<section id="three" class="spotlight style3 left">
+					<span class="image fit main bottom"><img src="images/pic04.jpg" alt="" /></span>
+					<div class="content">
+						<header>
+							<h2>LEADERSHIP</h2>
+							<p>Space Cardan Bike USA</p>
+						</header>
+						<p>We take pride in leading the way in providing cutting-edge, environmentaly friendly transportation options.</p>
+						<!--<ul class="actions">
+							<li><a href="#" class="button">Learn More</a></li>
+						</ul>-->
+					</div>
+					<a href="#four" class="goto-next scrolly">Next</a>
+				</section>
+
+			<!-- Four -->
+				<section id="four" class="wrapper style1 special fade-up">
+					<div class="container">
+						<header class="major">
+							<h2>RELIABILITY</h2>
+							<p>Space Cardan Bike USA</p>
+						</header>
+						
+						
+					    <br />
+						<div class="box alt">
+							<div class="row uniform">
+								<section class="4u 6u(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-area-chart"></span>-->
+									<div class="zoom_img"><img src="images/bikeRack5.jpg" alt="" /></div>
+									<h3>Durability</h3>
+									<p>Unlike chains and derailleurs that are fully exposed, the rugged and fully enclosed controction of the our drive shaft and hub can withstand riding conditions and impacts that would cripple chain bikes.</p>
+								</section>
+								<section class="4u 6u$(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-comment"></span>-->
+                                    <div class="zoom_img"><img src="images/bikeRack4.jpg" alt="" /></div>
+									<h3>Clean</h3>
+									<p>Because of the enclosure, there is no grease that would dirty riders and their clothing. Riders are clean and happy, not sent home with greasy reminders of an unpleasant experience.</p>
+									
+								</section>
+								<section class="4u$ 6u(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-flask"></span>-->
+                                    <div class="zoom_img"><img src="images/bikeRack3.jpg" alt="" /></div>
+									<h3>Safety</h3>
+									<p>With our chainless bikes, your riding is worry-free. All moving parts are fully enclosed so it won't tear clothes, get grease on your hands or clothes, or cause injuries. No more chain-bite. No more chain grease.</p>
+								</section>
+								<section class="4u 6u$(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-paper-plane"></span>-->
+                                    <div class="zoom_img"><img src="images/bikeRack6.jpg" alt="" /></div>
+									<h3>Less Maintenance</h3>
+									<p>We utilize a revolutionary drive shaft system for dramatically improved durability and performance. The fully enclosed drive shaft system keeps the parts protected from damage and rust from the elements. Our solution does not require any tune-ups or adjustments. The hubs are sealed and lifetime lubricated and don't have a maintenance schedule.</p>
+									
+									
+								</section>
+								<section class="4u 6u(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-file"></span>-->
+                                    <div class="zoom_img"><img src="images/bikeRack2.jpg" alt="" /></div>
+									<h3>Smarter Gearing</h3>
+									<p>We use advanced and highly efficient internal gear hubs, which compress all the moving parts into a compact gear hub inside the rear wheel.  These hubs eliminate all the overlapping and redundant gears of chain-based systems to give you the gear range you need, but in fewer steps. So shifting is simpler, and more consistent.</p>
+								</section>
+								<section class="4u$ 6u$(medium) 12u$(xsmall)">
+									<!--<span class="icon alt major fa-lock"></span>-->
+                                    <div class="zoom_img"><img src="images/bikeRack1.jpg" alt="" /></div>
+									<h3>Always in the Right Gear</h3>
+									<p>With our chainless bikes, shifting is independant of pedaling.  This means you can change gears at any time - while pedaling, coasting or even at a complete stop. This is further simplified by the gear indicator which shows you at a glance what gear you are in.  Why take your eyes off of what is in front of you to look down at the position of the chain?</p>
+								</section>
+							</div>
+						</div>
+						<footer class="major">
+							<ul class="actions">
+								<li><a href="#" class="button">Space Cardan</a></li>
+							</ul>
+						</footer>
+					</div>
+				</section>
+
+			<!-- Five -->
+				<section id="five" class="wrapper style2 special fade">
+					<div class="container">
+					    
+                        <?php 
+                            $email=$_REQUEST['email']; 
+                            if ($email == "") {
+                                echo "<header><h2>Want to know more?</h2><p>Sign up for additional information or call <ul class='actions'><li><a href='tel:+1-954-865-1434' class='button'>(954)865-1434</a></li></ul></p></header>";
+                                echo "<form method='POST' action='index.php' class='container 50%' enctype='multipart/form-data'><div class='row uniform 50%'>";
+                                echo "<div class='8u 12u$(xsmall)'><input type='email' name='email' id='email' placeholder='Your Email Address' /></div>";
+                                echo "<div class='4u$ 12u$(xsmall)'><input type='submit' value='Get Started' class='button' /></div></div></form>";
+                            } else {
+                                $name="Website Visitor";
+                                $message="Please send me more information";
+                                $from="From: $name<$email>\r\nReturn-path: $email"; 
+                                $subject="Space Cardan Website additional info"; 
+                                mail("spacecardanbike@gmail.com", $subject, $message, $from); 
+                                echo "<header><h2>Thank you for your interest!</h2></header>";      
+                            }
+                        ?> 
+						
+						
+                    
+				    
+                        
+				</div>
+				</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/spacecardanbikeusa/" target="_blank" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.linkedin.com/in/gina-brin-2488a511a" target="_blank" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						<li><a href="https://www.instagram.com/g.brin/" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						
+						<li><a href="mailto:spacecardanbike@gmail.com?subject=WebsiteEmail" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<div class="slider">
+                            <div class="viewer">
+                                <div class="reel">
+                                    <div class="slide">
+                                        <img src="images/bike1-alt.jpg" alt="" />
+                                    </div>
+                                    <div class="slide">
+                                        <img src="images/bike2-alt.jpg" alt="" />
+                                    </div>
+                                    <div class="slide">
+                                        <img src="images/bike3-alt.jpg" alt="" />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+					<ul class="copyright">
+						<li>&copy; Space Cardan Bike, USA. All rights reserved.</li>
+                        <!--<li>Design: <a href="http://html5up.net">HTML5 UP</a> | Adaptation: <a href="http://slickwebstudio.com">Slick Web Studio</a></li>-->
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+		    
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/jquery.slidertron.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+           
+            <script type="text/javascript">
+                $('.slider').slidertron({
+                    viewerSelector: '.viewer',
+                    reelSelector: '.viewer .reel',
+                    slidesSelector: '.viewer .reel .slide',
+                    advanceDelay: 5000,
+                    speed: 'slow'
+                });
+            </script>
+	</body>
+</html>
